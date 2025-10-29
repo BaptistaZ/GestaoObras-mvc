185d9ce838385db651df55aebaf52385948bb29c
BaptistaZ <tiagobaptista@ipvc.pt>
2025-10-29 15:42:33 +0000

    feat(web): adicionar CRUD completo de Clientes (controller + views + validações + menu)

--- GestaoObras.Domain/Entities/Cliente.cs
+++ GestaoObras.Domain/Entities/Cliente.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GestaoObras.Domain.Entities;
 
 public class Cliente
 {
     public int Id { get; set; }
+
+    [Required, StringLength(120)]
     public string Nome { get; set; } = null!;
+
+    [Required, StringLength(9, MinimumLength = 9, ErrorMessage = "O NIF deve ter 9 dígitos")]
+    [RegularExpression(@"^\d{9}$", ErrorMessage = "O NIF deve ter 9 dígitos")]
     public string NIF { get; set; } = null!;
+
+    [Required, StringLength(200)]
     public string Morada { get; set; } = null!;
+
+    [Required, EmailAddress, StringLength(150)]
     public string Email { get; set; } = null!;
+
+    [Required, Phone, StringLength(20)]
     public string Telefone { get; set; } = null!;
 
     public ICollection<Obra> Obras { get; set; } = new List<Obra>();
